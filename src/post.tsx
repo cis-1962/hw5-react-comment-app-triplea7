352581220db201930feac76e086539407a976721
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 22:28:22 -0400

    let's see if this works

--- src/post.tsx
+++ src/post.tsx
@@ -1,29 +1,25 @@
 import { useState } from 'react';
+import './src/style.css';
 
 export default function Post() {
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
   return (
-    <>
-      <head>
-        <link rel="stylesheet" type="text/css" href="src/style.css" />
-      </head>
-      <div className="post">
-        <h1>New Post</h1>
-        <input
-          type="text"
-          value={nameInput}
-          placeholder="Your name here"
-          onChange={(e) => enterNameInput(e.target.value)}
-        />
-        <input
-          type="text"
-          value={bodyInput}
-          placeholder="Speak your truth..."
-          onChange={(ev) => enterBodyInput(ev.target.value)}
-        />
-        <button type="submit">Post</button>
-      </div>
-    </>
+    <div className="post">
+      <h1>New Post</h1>
+      <input
+        type="text"
+        value={nameInput}
+        placeholder="Your name here"
+        onChange={(e) => enterNameInput(e.target.value)}
+      />
+      <input
+        type="text"
+        value={bodyInput}
+        placeholder="Speak your truth..."
+        onChange={(ev) => enterBodyInput(ev.target.value)}
+      />
+      <button type="submit">Post</button>
+    </div>
   );
 }
