ee1c116aa777f4479f6bb214fa6bff0f95f32174
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 21:56:20 -0400

    trying again again

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,6 @@ import App from './app';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
-    <head>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="/Users/alyssaantonian/Desktop/cis1962/hw5-react-comment-app-triplea7-main/src/style.css
-    "
-      />
-    </head>
     <App />
   </React.StrictMode>,
 );
--- src/post.tsx
+++ src/post.tsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
+import styles from 'src/style.module.css';
 
 export default function Post() {
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
   return (
-    <div className="post">
-      <h1>New Post</h1>
-      <input
-        type="text"
-        value={nameInput}
-        placeholder="Your name here"
-        onChange={(e) => enterNameInput(e.target.value)}
-      />
-      <input
-        type="text"
-        value={bodyInput}
-        placeholder="Speak your truth..."
-        onChange={(ev) => enterBodyInput(ev.target.value)}
-      />
-      <button type="submit">Post</button>
-    </div>
+    <>
+      <head>
+        <link rel="stylesheet" type="text/css" href="src/style.module.css" />
+      </head>
+      <div className={styles.post}>
+        <h1>New Post</h1>
+        <input
+          type="text"
+          value={nameInput}
+          placeholder="Your name here"
+          onChange={(e) => enterNameInput(e.target.value)}
+        />
+        <input
+          type="text"
+          value={bodyInput}
+          placeholder="Speak your truth..."
+          onChange={(ev) => enterBodyInput(ev.target.value)}
+        />
+        <button type="submit">Post</button>
+      </div>
+    </>
   );
 }
