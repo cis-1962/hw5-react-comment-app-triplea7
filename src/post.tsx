737277bb4f46fee57f041985708dc990a73f7314
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 23:30:46 -0400

    Add files via upload

--- src/post.tsx
+++ src/post.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 
 export default function PostHeader() {
+  let saveName;
+  let saveBody;
+
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
   return (
     <div className="post">
-      <h1>Testing Testing...</h1>
+      <h1>New Post</h1>
       <div>
         <input
           type="text"
           value={nameInput}
           placeholder="Your name here"
-          onChange={(e) => enterNameInput(e.target.value)}
+          onChange={(e) => {
+            enterNameInput(e.target.value);
+            saveName = e.target.value;
+          }}
         />
       </div>
       <div>
@@ -19,17 +25,20 @@ export default function PostHeader() {
           type="text"
           value={bodyInput}
           placeholder="Speak your truth..."
-          onChange={(ev) => enterBodyInput(ev.target.value)}
+          onChange={(ev) => {
+            enterBodyInput(ev.target.value);
+            saveBody = ev.target.value;
+          }}
         />
       </div>
-      <button type="submit" onClick={() => Post(nameInput, bodyInput)}>
+      <button type="submit" onClick={() => Post(saveName, saveBody)}>
         Post
       </button>
     </div>
   );
 }
 
-export function Post(name: string, body: string) {
+function Post(name, body) {
   PostHeader();
   return (
     <div className="post">
