ea281e0d7c49ed30fc68e0c61ac9485149ec8954
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 23:03:01 -0400

    i messed that one up my bad

---
+++ app.tsx
@@ -0,0 +1,5 @@
+import PostHeader from './post';
+
+export default function App() {
+  return PostHeader();
+}
--- post.tsx
+++ post.tsx
@@ -1,25 +1,40 @@
 import { useState } from 'react';
-import './src/style.css';
 
-export default function Post() {
+export default function PostHeader() {
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
   return (
     <div className="post">
       <h1>New Post</h1>
-      <input
-        type="text"
-        value={nameInput}
-        placeholder="Your name here"
-        onChange={(e) => enterNameInput(e.target.value)}
-      />
-      <input
-        type="text"
-        value={bodyInput}
-        placeholder="Speak your truth..."
-        onChange={(ev) => enterBodyInput(ev.target.value)}
-      />
-      <button type="submit">Post</button>
+      <div>
+        <input
+          type="text"
+          value={nameInput}
+          placeholder="Your name here"
+          onChange={(e) => enterNameInput(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={bodyInput}
+          placeholder="Speak your truth..."
+          onChange={(ev) => enterBodyInput(ev.target.value)}
+        />
+      </div>
+      <button type="submit" onClick={() => Post(nameInput, bodyInput)}>
+        Post
+      </button>
+    </div>
+  );
+}
+
+export function Post(name: string, body: string) {
+  PostHeader();
+  return (
+    <div className="post">
+      <div> {name}</div>
+      <div> {body}</div>
     </div>
   );
 }
