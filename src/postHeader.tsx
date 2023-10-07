0a298bb061b3df31dad2022326949388539ad685
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-07 02:46:55 -0400

    Add files via upload

--- src/postHeader.tsx
+++ src/postHeader.tsx
@@ -2,43 +2,48 @@ import { useState } from 'react';
 // eslint-disable-next-line import/no-cycle
 import Post from './post';
 
+let post;
+
 export default function PostHeader(inDepth: number) {
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
 
   return (
-    <div className="post">
-      <div>
-        <input
-          type="text"
-          value={nameInput}
-          placeholder="Your name here"
-          onChange={(e) => {
-            enterNameInput(e.target.value);
+    <>
+      <div className="post">
+        <div>
+          <input
+            type="text"
+            value={nameInput}
+            placeholder="Your name here"
+            onChange={(e) => {
+              enterNameInput(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={bodyInput}
+            placeholder="Speak your truth..."
+            onChange={(ev) => {
+              enterBodyInput(ev.target.value);
+            }}
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={() => {
+            const props = { name: nameInput, body: bodyInput, depth: inDepth };
+            post = Post(props);
+            enterNameInput('');
+            enterBodyInput('');
           }}
-        />
+        >
+          Post
+        </button>
       </div>
-      <div>
-        <input
-          type="text"
-          value={bodyInput}
-          placeholder="Speak your truth..."
-          onChange={(ev) => {
-            enterBodyInput(ev.target.value);
-          }}
-        />
-      </div>
-      <button
-        type="submit"
-        onClick={() => {
-          const post = { name: nameInput, body: bodyInput, depth: inDepth };
-          Post(post);
-          enterNameInput('');
-          enterBodyInput('');
-        }}
-      >
-        Post
-      </button>
-    </div>
+      <div>{post}</div>
+    </>
   );
 }
