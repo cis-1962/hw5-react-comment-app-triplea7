3a9e50fed4176e5905a0859196f4b51556a35a45
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 23:35:04 -0400

    add posts

--- src/post.tsx
+++ src/post.tsx
@@ -3,38 +3,47 @@ import { useState } from 'react';
 export default function PostHeader() {
   let saveName;
   let saveBody;
+  let additionalElements;
 
   const [nameInput, enterNameInput] = useState('');
   const [bodyInput, enterBodyInput] = useState('');
   return (
-    <div className="post">
-      <h1>New Post</h1>
-      <div>
-        <input
-          type="text"
-          value={nameInput}
-          placeholder="Your name here"
-          onChange={(e) => {
-            enterNameInput(e.target.value);
-            saveName = e.target.value;
+    <>
+      <div className="post">
+        <h1>New Post</h1>
+        <div>
+          <input
+            type="text"
+            value={nameInput}
+            placeholder="Your name here"
+            onChange={(e) => {
+              enterNameInput(e.target.value);
+              saveName = e.target.value;
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={bodyInput}
+            placeholder="Speak your truth..."
+            onChange={(ev) => {
+              enterBodyInput(ev.target.value);
+              saveBody = ev.target.value;
+            }}
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={() => {
+            additionalElements = Post(saveName, saveBody);
           }}
-        />
+        >
+          Post
+        </button>
       </div>
-      <div>
-        <input
-          type="text"
-          value={bodyInput}
-          placeholder="Speak your truth..."
-          onChange={(ev) => {
-            enterBodyInput(ev.target.value);
-            saveBody = ev.target.value;
-          }}
-        />
-      </div>
-      <button type="submit" onClick={() => Post(saveName, saveBody)}>
-        Post
-      </button>
-    </div>
+      <div className="post"> {additionalElements}</div>
+    </>
   );
 }
 
