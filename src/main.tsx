72a842c5a50000d6c22881deaf517ed1558419f9
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 21:33:40 -0400

    still  trying to make style work

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from './app';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
-    <link rel="stylesheet" type="text/css" href="../src/main.css" />
+    <head>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="/Users/alyssaantonian/Desktop/cis1962/hw5-react-comment-app-triplea7-main/src/style.css
+    "
+      />
+    </head>
     <App />
   </React.StrictMode>,
 );
