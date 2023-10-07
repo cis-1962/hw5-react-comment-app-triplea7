2ca138adc89756502e6c3669923bbb849cfe1440
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-06 21:23:05 -0400

    Add files via upload

--- src/post.tsx
+++ src/post.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Post() {
-    const [nameInput, enterNameInput] = useState('');
-    const [bodyInput, enterBodyInput] = useState('');
-    return (
-        <><h1>New Post</h1><><input
-            type="text"
-            value={nameInput}
-            onChange={(e) => enterNameInput(e.target.value)} />
-            <input
-                type="text"
-                value={bodyInput}
-                onChange={(ev) => enterBodyInput(ev.target.value)} /></></>
-    );
-};
+  const [nameInput, enterNameInput] = useState('');
+  const [bodyInput, enterBodyInput] = useState('');
+  return (
+    <div className="post">
+      <h1>New Post</h1>
+      <input
+        type="text"
+        value={nameInput}
+        placeholder="Your name here"
+        onChange={(e) => enterNameInput(e.target.value)}
+      />
+      <input
+        type="text"
+        value={bodyInput}
+        placeholder="Speak your truth..."
+        onChange={(ev) => enterBodyInput(ev.target.value)}
+      />
+      <button type="submit">Post</button>
+    </div>
+  );
+}
