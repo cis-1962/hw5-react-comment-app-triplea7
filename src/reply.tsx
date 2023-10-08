2fee9a58b4a38d701d886e962208b1becf9eb72b
Alyssa Antonian <116849690+triplea7@users.noreply.github.com>
2023-10-08 02:41:36 -0500

    reply input boxes only appear if u hit reply

--- src/reply.tsx
+++ src/reply.tsx
@@ -12,48 +12,57 @@ export interface ReplyProps {
 export default function Reply({ post, whenReply }: ReplyProps) {
   const [replier, setReplier] = useState('');
   const [replyBody, setReplyBody] = useState('');
+  const [showInputs, setShowInputs] = useState(false);
 
   return (
     <div>
       <p>
-        <strong>{post.name}:</strong>
+        <strong>{post.name}: </strong>
         {post.body}
       </p>
-      <div>
-        <input
-          type="text"
-          placeholder="Your name here"
-          onChange={(e) => {
-            setReplier(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          value={replyBody}
-          placeholder="Speak your truth..."
-          onChange={(ev) => {
-            setReplyBody(ev.target.value);
-          }}
-        />
-      </div>
-      <button
-        type="submit"
-        onClick={() => {
-          whenReply(replier, replyBody);
-          setReplier('');
-          setReplyBody('');
-        }}
-      >
-        Reply
-      </button>
-      <div>
-        {post.replies.map((reply, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Reply key={index} post={reply} whenReply={whenReply} />
-        ))}
-      </div>
+      {!showInputs ? (
+        <button type="submit" onClick={() => setShowInputs(true)}>
+          Reply
+        </button>
+      ) : (
+        <>
+          <div>
+            <input
+              type="text"
+              placeholder="Your name here"
+              onChange={(e) => {
+                setReplier(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={replyBody}
+              placeholder="Speak your truth..."
+              onChange={(ev) => {
+                setReplyBody(ev.target.value);
+              }}
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={() => {
+              whenReply(replier, replyBody);
+              setReplier('');
+              setReplyBody('');
+            }}
+          >
+            Post
+          </button>
+          <div>
+            {post.replies.map((reply, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Reply key={index} post={reply} whenReply={whenReply} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
